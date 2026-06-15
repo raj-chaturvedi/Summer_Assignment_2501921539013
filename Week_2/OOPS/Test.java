4c4fe11e8423d6783b847bef9e052587f3365b5a
raj-chaturvedi <24113lt276@glbitm.ac.in>
2026-06-15 13:38:34 +0530

    Update Test.java

--- Week_2/OOPS/Test.java
+++ Week_2/OOPS/Test.java
@@ -1,1 +1,28 @@
+package live;
 
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // (a) Veena object
+        Veena veena = new Veena();
+        veena.play();
+
+        // (b) Saxophone object
+        Saxophone saxophone = new Saxophone();
+        saxophone.play();
+
+        // (c) Using Playable reference
+        Playable instrument;
+
+        instrument = veena;
+        instrument.play();
+
+        instrument = saxophone;
+        instrument.play();
+    }
+}
