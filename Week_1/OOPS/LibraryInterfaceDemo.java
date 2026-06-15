bb2319a018554a106c4222d76eafb3700204ebbd
raj-chaturvedi <24113lt276@glbitm.ac.in>
2026-06-15 12:42:45 +0530

    Update LibraryInterfaceDemo.java

--- Week_1/OOPS/LibraryInterfaceDemo.java
+++ Week_1/OOPS/LibraryInterfaceDemo.java
@@ -1,1 +1,37 @@
+public class LibraryInterfaceDemo {
 
+    public static void main(String[] args) {
+
+        System.out.println("===== Kid User Test =====");
+
+        KidUser kid1 = new KidUser();
+        kid1.age = 10;
+        kid1.registerAccount();
+
+        kid1.bookType = "Kids";
+        kid1.requestBook();
+
+        KidUser kid2 = new KidUser();
+        kid2.age = 18;
+        kid2.registerAccount();
+
+        kid2.bookType = "Fiction";
+        kid2.requestBook();
+
+        System.out.println("\n===== Adult User Test =====");
+
+        AdultUser adult1 = new AdultUser();
+        adult1.age = 5;
+        adult1.registerAccount();
+
+        adult1.bookType = "Kids";
+        adult1.requestBook();
+
+        AdultUser adult2 = new AdultUser();
+        adult2.age = 23;
+        adult2.registerAccount();
+
+        adult2.bookType = "Fiction";
+        adult2.requestBook();
+    }
+}
